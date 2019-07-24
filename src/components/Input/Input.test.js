8a320d885ee178c2d5b8e594d4a5cf0976895316
Joey Leger <joeyleger@Joeys-MacBook-Pro.local>
2019-07-24 14:32:02 -0400

    [ADD] Added enzyme to the testing

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,20 +1,31 @@
-import { Input } from './Input'; 
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Input } from './Input';
 
-describe('Input Fields', () => {
+configure({ adapter: new Adapter() });
 
-    it('Should render correctly', () => {
-        
-    });
+describe('Input', () => {
+    const testProps = {
+        errors: {
+            streetAddress: false
+        },
+        label: 'streetAddress',
+        errorMessage: ''
+    };
+    const input = shallow(<Input { ...testProps } />);
+
+    // it('should render correctly', () => {
+    //     expect(input).toMatchSnapshot();
+    // });
 
     it('Should show error message if field is empty', () => {
-        
     });
 
     it('Should hide error message if field is not empty', () => {
-
     });
 
     it('Should only let valid zipcodes pass', () => {
-
     });
+
 });
