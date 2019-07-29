081937aaad0f6bc1f3f8e3483c1c2bca22078945
Joey Leger <joeyleger@Joeys-MacBook-Pro.local>
2019-07-29 09:18:16 -0400

    [FIX] Fixed imports for tests

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { Input } from './Input';
+import Input from './Input';
 
 configure({ adapter: new Adapter() });
 
