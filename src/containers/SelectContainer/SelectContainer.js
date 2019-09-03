310791771d4b2d3224f661b2e5b1e24ec0349987
Joey Leger <joeyleger@Joeys-MacBook-Pro.local>
2019-09-03 11:38:55 -0400

    added error border color to select field. added defaultOption select prop

--- src/containers/SelectContainer/SelectContainer.js
+++ src/containers/SelectContainer/SelectContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Select from '../../components/Select/Select';
 import { states } from '../../utils/Helper';
 import './SelectContainer.scss';
@@ -7,11 +7,23 @@ import './SelectContainer.scss';
 const SelectContainer = props => {
 
     const dispatch = useDispatch();
+    const errors = useSelector(state => state.Input.errors);
+    const { defaultOption } = props;
+
+    const checkValue = target => {
+        // Check for blank values
+        if (target.value === defaultOption ?
+            dispatch({ type: 'SHOW_INPUT_ERROR', errorName: props.label }) :
+            dispatch({ type: 'HIDE_INPUT_ERROR', errorName: props.label })
+        );
+    };
 
     return (
         <Select
             value={ props.value }
-            setDefaultValue='Select A State'
+            setDefaultValue={ defaultOption }
+            onBlur={ e => { checkValue(e.target) }}
+            error={ errors[props.label] }
             onChange={ e => { dispatch({ type: 'SAVE_VALUE', stateName: 'state', stateValue: e.target.value }) }}
             options={ states.names.map(name => (<option key={ name } value={ name }>{ name }</option>)) }
         />
