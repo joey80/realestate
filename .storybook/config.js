d11ceac02a8951ee62dd7832df0a025429729baa
Joey <legerinvestments@gmail.com>
2019-09-02 20:35:35 -0400

    added wrapper for storybook. added select props

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,16 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import '../src/containers/Form/Form.scss';
+
+addDecorator(storyFn => (
+  <div className="form">
+    <div className="form__container">
+      <div className="form__section">
+        { storyFn() }
+      </div>
+    </div>
+  </div>
+));
 
 function loadStories() {
   require('../src/stories');
--- src/stories/index.js
+++ src/stories/index.js
@@ -5,7 +5,7 @@ import { storiesOf } from '@storybook/react';
 import Input from '../components/Input/Input';
 import Loader from '../components/Loader/Loader';
 import Select from '../components/Select/Select';
-
+import { states } from '../utils/Helper';
 
 storiesOf('Input', module)
   .add('default', () => <Input />);
@@ -14,4 +14,7 @@ storiesOf('Loader', module)
   .add('default', () => <Loader />);
 
 storiesOf('Select', module)
-  .add('default', () => <Select />);
+  .add('default', () => (
+    <Select
+      options={ states.names.map(name => (<option key={ name } value={ name }>{ name }</option>)) }/>
+  ));
