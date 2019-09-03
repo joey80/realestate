f0f4296724f7a2a8cd29646da3a885e2398f78d4
Joey <legerinvestments@gmail.com>
2019-09-03 16:39:59 -0400

    added working tests

--- src/containers/SelectContainer/SelectContainer.test.js
+++ src/containers/SelectContainer/SelectContainer.test.js
@@ -5,14 +5,14 @@ import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import InputContainer from './InputContainer';
+import SelectContainer from './SelectContainer';
 
 configure({ adapter: new Adapter() });
 
 const mockStore = configureMockStore([thunk]);
 let wrapper;
 
-describe('<InputContainer />', () => {
+describe('<SelectContainer />', () => {
   
   beforeEach(() => {
     const store = mockStore({
@@ -32,22 +32,22 @@ describe('<InputContainer />', () => {
     });
 
     const props = {
-      label: "streetAddress",
-      name: "Street Address",
-      type: "text",
-      errorMessage: "Please enter a street address"
+        defaultOption: 'Select A State',
+        value: 'Select A State',
+        label: 'state',
+        errorMessage: 'Please choose a state'
     };
 
     wrapper = mount(
       <Provider store={ store }>
-        <InputContainer { ...props } />
+        <SelectContainer { ...props } />
       </Provider>
     );
 
   });
 
   it('renders correctly', () => {
-    expect(wrapper.find(InputContainer).length).to.equal(1);
+    expect(wrapper.find(SelectContainer).length).to.equal(1);
   });
 
 });
