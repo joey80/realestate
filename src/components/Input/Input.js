0048c31fd4c10214229384e31e52fa974fd00e3c
Joey Leger <legerinvestments@gmail.com>
2020-01-03 23:56:30 -0500

    prettied the formatting

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Input.scss';
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Input.scss'
 
 const Input = props => (
-
-    <input
-        value={ props.value }
-        type={ props.type }
-        onChange={ props.onChange }
-        onBlur={ props.onBlur }
-        name={ props.label }
-        className={( props.error ?
-            'input__field input__field--error' :
-            'input__field'
-        )}
-    />
-);
+  <input
+    value={props.value}
+    type={props.type}
+    onChange={props.onChange}
+    onBlur={props.onBlur}
+    name={props.label}
+    className={props.error ? 'input__field input__field--error' : 'input__field'}
+  />
+)
 
 Input.propTypes = {
-    name: PropTypes.string,
-    type: PropTypes.string
-};
+  name: PropTypes.string,
+  type: PropTypes.string
+}
 
-export default Input;
+export default Input
