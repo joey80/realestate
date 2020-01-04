482ee59f231e6b176f4f1a3f24077cb70b02db6f
Joey Leger <legerinvestments@gmail.com>
2020-01-04 00:14:40 -0500

    prettied and destructured props on InputError

--- src/components/InputError/InputError.js
+++ src/components/InputError/InputError.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './InputError.scss';
+import React from 'react'
+import PropTypes from 'prop-types'
+import './InputError.scss'
 
-const InputError = props => {
-    return (props.error ?
-        <span className="inputError__message">{ props.errorMessage }</span> :
-        <span className="inputError__message" />
-    );
-};
-
-InputError.propTypes = {
-    error: PropTypes.bool,
-    errorMessage: PropTypes.string
+const InputError = ({ error, errorMessage }) => {
+  return error ? (
+    <span className="inputError__message">{errorMessage}</span>
+  ) : (
+    <span className="inputError__message" />
+  )
 }
 
-export default InputError;
+InputError.propTypes = {
+  error: PropTypes.bool,
+  errorMessage: PropTypes.string
+}
+
+export default InputError
