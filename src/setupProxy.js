7eb3fff352d8d4b71e71719691d12af3ce051185
Joey Leger <joseph.leger@duke-energy.com>
2021-03-24 22:13:58 -0400

    fix: cleaned up setupProxy path rewrite

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,14 @@
 const createProxyMiddleware = require('http-proxy-middleware');
 
-const rewriteFn = function (path, req) {
-  const id = process.env.REACT_APP_ZILLOW_ZWSID;
-  const { address, citystatezip } = req.query;
-  return `http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=${id}&address=${address}&citystatezip=${citystatezip}`;
-};
-
 module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
       target: 'http://www.zillow.com',
       changeOrigin: true,
-      pathRewrite: rewriteFn,
+      pathRewrite: {
+        '^/api/': '/',
+      },
     })
   );
 };
